88350146b3c8db51623dc64daf4717f2c36d1ed4
Mustafa Saify <saifym@advisory.com>
2016-02-07 22:13:54 +0530

    FEE BOOTCAMP :
    currencyConverter :
    note - created separate files as i want clean css and js to quickly ship this code to react
    functionality- added functionality on changing currency dropdowns or adding text , the to be converted currency calculation.
    
    stockTicker:
    completed search functionlity and error handling for stock ticker

---
+++ assets/app/js/StockTicker.js
@@ -0,0 +1,75 @@
+﻿var source = $("#stockTemplate").html();
+var template = Handlebars.compile(source);
+
+$(document).ready(function() {
+
+    $('#getquote').bind("click", function() {
+
+        var ticker = $('#stockinput').val();
+
+        var today = new Date();
+        today.setDate(today.getDate() - 3);
+        var yesterday = new Date(today);
+        yesterday.setDate(today.getDate() - 1);
+        var formattedTodayDate = today.toISOString().slice(0, 10);
+        var formattedYesterdayDate = yesterday.toISOString().slice(0, 10);
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://stockvider.p.mashape.com/indicator/DATA/" + ticker + '/?end_date=' + formattedTodayDate + '&start_date=' + formattedYesterdayDate,
+            "method": "GET",
+            "headers": {
+                "content-type": "application/csv",
+                "x-mashape-key": "Bs5BvTwMeNmshIVgyxatfWRfPMkNp1Dmi30jsnLUNZ8zyDyBW8",
+                "cache-control": "no-cache"
+            }
+        }
+
+        $.ajax(settings).done(function(response) {
+
+            if (response.Error !== undefined) {
+
+                $('#searchstockResult').html('<font color="red">' + response.Error + '</font>');
+
+            } else {
+
+                var result;
+
+                if (response.Dataset === undefined) {
+
+                    result = "No Updates on Stock Market Today";
+
+                } else {
+
+                    var today = response.Dataset[0].High;
+                    var previous = response.Dataset[1].High;
+
+                    result = template({
+                        Today: today,
+                        Previous: previous
+                    });
+
+                    
+                }
+               
+                $('#searchstockResult').html(result);
+            }
+           
+        });
+    });
+        $('#getcurrency').bind("click", function() {
+
+            var inputcurrencyAmt = $('#inputcurrency').val();
+
+            var inrRate = currencydata.to_amount;
+
+            var result = currencytemplate({
+                currencyresult: inrRate * inputcurrencyAmt
+            });
+            $('#getcurrencyresult').html(result);
+        });
+
+});
+
+
---
+++ assets/app/js/currencyConverter.js
@@ -0,0 +1,74 @@
+﻿var template;
+
+$(document).ready(function () {
+
+    $.ajax({
+        url: 'https://currencyconverter.p.mashape.com/availablecurrencies/',
+        dataType: 'json',
+        headers: {
+            'X-Mashape-Key': 'Bs5BvTwMeNmshIVgyxatfWRfPMkNp1Dmi30jsnLUNZ8zyDyBW8'
+        }
+    })
+	.done(function (data) {
+
+	    for (var i = 0; i < data.length; i++) {
+	        var elem = document.createElement('option');
+	        $('.currency-selecter')
+			.append($(elem)
+			.attr('value', data[i].id).text(data[i].description.slice(4)));
+	    }
+	});
+
+    $('.currency-From').on('keyup', function (e) {
+        clearTimeout($.data(this, 'timer'));
+        if (e.keyCode == 13) {
+            getConvertedCurrencyVal();
+        }
+        else
+            $(this).data('timer', setTimeout(getConvertedCurrencyVal, 200));
+    });
+
+    $(".js-currencyFrom").change(function () {
+        getConvertedCurrencyVal();
+    });
+
+    $(".js-currencyTo").change(function () {
+        getConvertedCurrencyVal();
+    });
+
+    function getConvertedCurrencyVal() {
+        var fromCurrency = $('.js-currencyFrom :selected').val();
+        var toCurrency = $('.js-currencyTo :selected').val();
+
+        var fromamount = $('.currency-From').val();
+
+        if (fromamount === '') {
+            $('.js-error-message').text("Please Enter Amount to be Converted!!");
+        } else {
+
+            $('.js-error-message').text("");
+            var amount = parseInt(fromamount);
+
+            $.ajax({
+                url: 'https://currencyconverter.p.mashape.com/?from=' + fromCurrency +
+                    '&from_amount=' + amount + '&to=' + toCurrency,
+                dataType: 'json',
+                headers: {
+                    'X-Mashape-Key': 'Bs5BvTwMeNmshIVgyxatfWRfPMkNp1Dmi30jsnLUNZ8zyDyBW8'
+                }
+            })
+           .done(function (data) {
+
+               if (data.error !== undefined) {
+
+                   $('.js-error-message').innerHTML("Invalid Input");
+
+               } else {
+
+                   $('.currency-converted-box').val(data.to_amount);
+               }
+           });
+        }
+       
+    }
+});
